27c725e3a57af9457844f9a67f99b6088959505b
saullocastro <saullogiovani@gmail.com>
2017-04-21 00:51:22 -0300

    Version: 0.15.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-VERSION = "0.10.0"
+VERSION = "0.15.0"
 
 setup(
     name = "mapy_package",
