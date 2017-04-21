1b9c5d0468722af629f42ddbe342d4462430d855
saullocastro <saullogiovani@gmail.com>
2017-04-21 00:37:34 -0300

    TST: tests

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,8 +1,13 @@
+import os
+import inspect
+
 from mapy.fem import FEM
+
+THIS_DIR = os.path.dirname(inspect.getfile(inspect.currentframe()))
 
 def test():
     fem = FEM()
-    fem.read_new_file('dummy_wing_metallic.bdf')
+    fem.read_new_file(os.path.join(THIS_DIR, 'dummy_wing_metallic.bdf'))
 
 if __name__ == '__main__':
     test()
